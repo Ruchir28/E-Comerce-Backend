cf883abe4f7fa703a6401ce23bf1d46bc6931caf
Ruchir <ruchirnakra28@gmail.com>
2021-04-20 23:02:59 +0530

    Signin Done

--- controllers/auth.js
+++ controllers/auth.js
@@ -1,6 +1,7 @@
  const User = require("../models/user");
 const {validationResult} = require('express-validator');
-
+var jwt = require('jsonwebtoken');
+var expressJwt = require('express-jwt');
 exports.signout = (req,res)=>{
     return res.json({
         message:"User Signed Out"
@@ -33,5 +34,67 @@ exports.signup = (req,res)=>{
          user   
         });
     })
+}
 
+exports.signin = (req,res) => {
+    const {email,password} = req.body;
+    const errors = validationResult(req);
+    if(!errors.isEmpty())
+    {
+        return res.status(422).json({
+            err:[...errors.array().map((e)=>e.msg)],
+            message:"Fill The Information correctly"
+        });
+    }
+    User.findOne({email},(err,user)=>{
+        if(err){
+            res.status(400).json({
+                error:"User Doesn't Exists"
+            })
+        }
+        if(user.authenticate(password))
+        {
+            //create token 
+            const token = jwt.sign({_id:user._id},process.env.SECRET);
+            const {name,email,_id,role} = user;
+            return res.json({
+                message:"Signed In",
+                user:{name,email,_id,role},
+                token
+            });
+        }
+        return res.status(401).json({
+            err:"Email/Password do not match"
+        })
+
+
+    })
+}
+
+exports.isSignedin = expressJwt({
+    secret:process.env.SECRET,
+    userProperty:"auth",
+    algorithms: ['HS256']
+});
+
+exports.isAuthenticated = (req,res,next) => { 
+    let checker = req.profile && req.auth && req.profile._id === req.auth._id;
+    if(!checker)
+    {
+        return res.status(403).json({
+            error:"Access Denied"
+        });
+    }
+    next();
+}
+
+exports.isAdmin = (req,res,next) => {
+    if(req.profile.role === 0)
+    {
+        return res.status(403).json({
+            error:"Access Denied"
+        });
+    }
+    next();
+    
 }
--- routes/auth.js
+++ routes/auth.js
@@ -1,5 +1,5 @@
 var express = require('express');
-const { signout, signup } = require('../controllers/auth');
+const { signout, signup,signin, isSignedin } = require('../controllers/auth');
 const {check,validationResult} = require('express-validator');
 
 var router = express.Router();
@@ -10,9 +10,22 @@ router.post("/signup",[
     check("email").isEmail().withMessage("Email is required"),
     check("password",).isLength({min:3}).withMessage("Password should be atleast of length 3"),
 
-],(req,res,next)=>{console.log('passed');next();},signup);
+],signup);
+
+
+router.post("/signin",[
+    check("email").isEmail().withMessage("Email is required"),
+    check("password",).isLength({min:3}).withMessage("Password should be atleast of length 3"),
+
+],signin);
 
 router.get('/signout',signout);
 
+router.get('/testroute',isSignedin,(req,res)=>{
+    console.log(req.auth);
+    return res.json({
+        message:"A Protected Route"
+    })
+})
 
 module.exports = router;
